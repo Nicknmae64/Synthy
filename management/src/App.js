f5b684f46010ce33a5fe7f0003c1dd9ab67127cb
Synthy <grace2sj.lee@gmail.com>
2024-01-16 17:20:34 +0900

    synthy

---
+++ management/src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="background">
+      <img src={logo} lat ="logo">
+        <h2> 윤태웅 바보</h2>
+      </img>
+    </div>
+  );
+}
+
+export default App;
